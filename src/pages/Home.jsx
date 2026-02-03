3fe97d7eb6b3144610809d0aed77c3a61e1f08db
Mirfeez <mahammadmirfeez@gmail.com>
2026-02-04 00:20:39 +0530

    first push

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+   return (
+		<section className="text-center py-20 px-6">
+			<h2 className="text-4xl font-bold mb-4">
+				We Build Digital Experiences
+			</h2>
+			<p className="text-gray-600 max-w-xl mx-auto">
+				Digipixeltech helps businesses grow with modern websites, apps and
+				digital solutions.
+			</p>
+
+			<button className="mt-6 px-6 py-3 bg-purple-600 text-white rounded-lg">
+				Get Started
+			</button>
+		</section>
+	);
+}
+
+export default Home;

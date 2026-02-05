e0018f37ea63aae5df4ff3c453a7f9d6119adb22
Silent1killer <planetearth68u@gmail.com>
2026-02-05 12:18:17 +0530

    v c

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,12 +12,12 @@ const Projects = () => {
       <div>
          <h1 className="my-20 text-5xl flex justify-center">Projects</h1>
          <div className="flex flex-row">
-            <div className="w-2/3">
+            <div className="w-3/5 h-210">
             <iframe src="https://haseeb-interior.netlify.app/" frameborder="0" className="iframe h-200 w-400 m-10"></iframe>
             </div>
-            <div className="w-1/3 mt-10 mr-10">
-               <h1 className="text-3xl">Interior Portfolio</h1>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum ea, odio quibusdam nulla amet culpa dolores ratione fugiat aperiam ipsa accusamus fuga vel mollitia neque nemo commodi fugit tenetur libero! Deserunt dolor minus beatae tempora atque facilis ea recusandae, porro maxime. Magni architecto deserunt aspernatur quo sit assumenda. Earum quas ad velit aliquam odit asperiores quo et! Ipsa corporis voluptate veniam, id repudiandae adipisci quam rem odio eum sed nesciunt natus quas earum omnis consequatur suscipit facere beatae voluptates sunt impedit blanditiis. Nostrum impedit recusandae, assumenda beatae vitae, laboriosam eaque, iure corporis ducimus quae similique maxime? At, recusandae quo.</p>
+            <div className="w-1/3 mt-8 mr-10">
+               <h1 className="text-3xl mb-5">Interior Portfolio</h1>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum ea, odio quibusdam nulla amet culpa dolores ratione fugiat aperiam ipsa accusamus fuga vel mollitia neque nemo commodi fugit tenetur libero! Deserunt dolor minus beatae tempora atque facilis ea recusandae, porro maxime. Magni architecto deserunt aspernatur quo sit assumenda. Earum quas ad velit aliquam odit asperiores quo et! Ipsa corporis voluptate veniam, id repudiandae adipisci quam rem odio eum sed nesciunt natus quas earum omnis consequatur suscipit facere beatae voluptates sunt impedit blanditiis. Nostrum impedit recusandae, assumenda beatae vitae, laboriosam eaque, iure corporis ducimus quae similique maxime? At, recusandae quo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt tenetur hic provident quisquam dignissimos debitis adipisci labore facilis libero ea, ex vero nihil aspernatur facere porro doloremque sequi. Minima, architecto? Fugiat aut labore repellendus modi rerum exercitationem tenetur reiciendis ipsum dignissimos, excepturi dicta obcaecati neque, atque quo id voluptatem magni vero ut cumque sunt fuga magnam soluta. Nisi tempora eos totam esse, asperiores quaerat magni, ratione id, voluptatibus amet et aut aliquid deleniti molestiae sapiente aliquam. Quia aut corrupti cumque, asperiores ex voluptatem enim at repellat porro dolores dolore saepe cum, doloremque veniam facere nulla! Veritatis asperiores tenetur eum voluptas?</p>
             </div>
          </div>
       </div>

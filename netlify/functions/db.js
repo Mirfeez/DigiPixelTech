078ef69230cdda6cc8fad77abb96893df4e44a19
digipixeltech <digipixeltechsolution@gmail.com>
2026-02-12 14:43:37 +0530

    Update db.js

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -5,14 +5,32 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
 });
 
-exports.handler = async function () {
+exports.handler = async function (event) {
   try {
-    const result = await pool.query("SELECT * FROM contact_form");
+    if (event.httpMethod === "POST") {
+      const { email } = JSON.parse(event.body);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result.rows),
-    };
+      const result = await pool.query(
+        "INSERT INTO contact_form (email) VALUES ($1) RETURNING *",
+        [email]
+      );
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result.rows[0]),
+      };
+    }
+
+    if (event.httpMethod === "GET") {
+      const result = await pool.query("SELECT * FROM contact_form");
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result.rows),
+      };
+    }
+
+    return { statusCode: 405 };
   } catch (error) {
     return {
       statusCode: 500,
